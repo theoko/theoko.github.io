6821c33c821fdf3be6f08c773414df3a7b5de740
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-02-25 18:09:16 -0500

    added materialize and react

---
+++ components/index.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const e = React.createElement;
+
+class Project extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { selected: false };
+    }
+
+    // renderProject = () => {
+
+    // }
+
+    render() {
+
+        // if (this.state.selected) {
+        //     return 'Selected project ' + this.props.projectID;
+        // }
+
+        // var test = (
+        //     <div class="row"> 
+        //         <div class="col s4"><img src={this.props.projectImage} /></div>
+        //         <div class="col s8">2</div>
+        //     </div>
+        // ) 
+
+        return e(
+            'div',
+            { 
+                className: 'row',
+                onClick: () => this.setState({ selected: true }) 
+            },
+
+            e(
+                'div',
+                {
+                    className: 'col s4'
+                },
+                e(
+                    'img',
+                    {
+                        src: this.props.projectImage,
+                        className: 'responsive-img'
+                    }
+                )
+            ),
+
+            e(
+                'div',
+                {
+                    className: 'col s8'
+                },
+                this.props.projectDescription
+            )
+        );
+
+    }
+}
+
+document.querySelectorAll('.project')
+    .forEach(domContainer => {
+
+        // Read the project ID from a data-* attribute.
+        const projectID = parseInt(domContainer.dataset.projectid, 10);
+        const projectImage = domContainer.dataset.projectimage;
+        const projectDescription = domContainer.dataset.projectdescription;
+
+        ReactDOM.render(
+            e(Project, {
+                projectID: projectID,
+                projectImage: projectImage,
+                projectDescription: projectDescription
+            }),
+
+            domContainer
+        );
+
+    });
